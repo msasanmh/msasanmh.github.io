164b8cc1933cc3ceab3ae0ca461d2edcc4099555
MSasanMH <msasanmh@gmail.com>
2020-02-25 03:53:09 +0330

    Update github-query.js

--- github-query.js
+++ github-query.js
@@ -1,5 +1,5 @@
 jQuery.githubUser = function(username, callback) {
-   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=rel',callback)
+   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
 }
  
 jQuery.fn.loadRepositories = function(username) {
